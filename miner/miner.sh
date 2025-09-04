624c445c46eb9f7d1e7176ce9188e1b2dc421a50
Aaron Ward <ward349@wright.edu>
2025-09-04 00:49:29 +0000

    miner.sh + coint.txt complete

--- miner/miner.sh
+++ miner/miner.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
 
-
-echo "Starting this script (hopefully it is executable with chmod a+x ./miner.sh)"
-
-VARIABLE=some_string
-
-echo $VARIABLE
-
-## Prints all words in provided dictionary
-## (you might want to find a bigger dictionary)
-for i in $(cat ../data/dictionary); do
-  printf $i
-done
-
-
-## prints all numbers between 100 and 105
-for i in $(seq 100 105); do
-  printf $i
-done
+# I may have taken your one liner and ran with it(maybe)
+# I had ChatGPT help me fix the formating of the one liner to clean it up
+# I did use some of the code that was in here to help me create a better one liner
+for j in $(cat ../data/dictionary); do # probably should have found a bigger dictionary
+   for i in $(seq 5000); do
+      string="$i$j";
+      hashval=$(printf "%s" "$string" | sha256sum | awk '{print $1}');
+      echo "$hashval, $string";
+   done;
+done | grep '^000'
 
 
